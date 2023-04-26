b1e66e937cab205d19b01ce83a9d6e83ceb1683a
Zmulvihill <zmulvihi@yahoo.com>
2023-04-26 13:33:39 -0700

    added project7 and saved

---
+++ project7.jsx
@@ -0,0 +1,10 @@
+//Create a Stateless Functional Component
+const MyComponent = function() {
+    // Change code below this line
+  return (
+    <div>Keep on going!</div>
+  );
+  
+  
+    // Change code above this line
+  };

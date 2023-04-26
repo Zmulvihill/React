610f6c7a9210bd26c8e4c777ca3d70cc97609d28
Zmulvihill <zmulvihi@yahoo.com>
2023-04-26 13:24:27 -0700

    added project6 and saved

---
+++ project6.jsx
@@ -0,0 +1,8 @@
+//Learn About Self-Closing JSX Tags
+const JSX = (
+    <div>
+      <h2>Welcome to React!</h2> <br /> {/* Added / to br to close it */}
+      <p>Be sure to close all tags!</p>
+      <hr /> {/* Added / to hr to close it */}
+    </div>
+  );

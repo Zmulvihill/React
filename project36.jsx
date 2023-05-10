7ee91827cee05ed8b618636b43335b7fc0e1a7b8
Zmulvihill <zmulvihi@yahoo.com>
2023-05-10 10:22:50 -0700

    added project36 and saved

---
+++ project36.jsx
@@ -0,0 +1,8 @@
+//Introducing Inline Styles
+class Colorful extends React.Component {
+    render() {
+      return (
+        <div style={{color: "red", fontSize: 72}}> Big Red</div>
+      );
+    }
+  };

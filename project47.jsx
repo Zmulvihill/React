f8c43e8eab81b0f8735d28b5b38a2f7c56ee8664
Zmulvihill <zmulvihi@yahoo.com>
2023-05-14 15:29:02 -0700

    added project47 and saved

---
+++ project47.jsx
@@ -0,0 +1,12 @@
+//Render React on the Server with renderToString
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return <div/>
+    }
+  };
+  
+  // Change code below this line
+  ReactDOMServer.renderToString(<App />);
